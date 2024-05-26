686f0ee575584326246890043acf085cb1d16f4c
Fernando <fer.gab.sua@gmail.com>
2024-05-26 00:36:11 -0300

    Update common.py

--- Backend/my_site/settings/common.py
+++ Backend/my_site/settings/common.py
@@ -37,11 +37,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'rest_framework_simplejwt',
-
-    #prueba spectacular_settings:
-    'drf_spectacular',
-    'drf_spectacular_sidecar',
-}
 ]
 
 MIDDLEWARE = [
@@ -147,16 +142,4 @@ REST_FRAMEWORK = {
     ),
     # YOUR SETTINGS
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-}
-
-SPECTACULAR_SETTINGS = {
-    'TITLE': 'Your Project API',
-    'DESCRIPTION': 'Your project description',
-    'VERSION': '1.0.0',
-    'SERVE_INCLUDE_SCHEMA': False,
-    # OTHER SETTINGS
-    'SWAGGER_UI_DIST': 'SIDECAR',  # shorthand to use the sidecar instead
-    'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
-    'REDOC_DIST': 'SIDECAR',
-
 }
