8ea0e8800a4a1ffd2e63cfd5244a4e01ef2c43dd
Raul Madero <r.madero.ramirez@gmail.com>
2024-05-30 13:32:52 -0600

    Header terminado

--- Frontend/src/app/index.jsx
+++ Frontend/src/app/index.jsx
@@ -1,6 +1,7 @@
 
 import { Landing } from "../pages/landing"
 import { Register } from "../pages/register";
+import { Header } from "../components/header";
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <>
         <Routes>
             <Route path="/" element={<Landing />} />
+            <Route path="/header" element={<Header />} />
             <Route path="/auth" >
               <Route path="register" element={<Register />} />
             </Route>
