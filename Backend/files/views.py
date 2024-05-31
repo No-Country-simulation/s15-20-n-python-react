bac2bc583c1b66eea141fae1e46f313e6b316c13
DamBeDev <143622448+DamBeDev@users.noreply.github.com>
2024-05-30 23:08:11 -0300

    Agregar lógica de vista para Cloudinary.

--- Backend/files/views.py
+++ Backend/files/views.py
@@ -2,6 +2,39 @@ from rest_framework import generics, permissions
 from files.serializers import FileSerializer, TaskSerializer
 from core.models import File, Task
 from drf_spectacular.utils import extend_schema
+from cloudinary import uploader
+from cloudinary import api
+
+class FileView(generics.ListCreateAPIView):
+    
+    @extend_schema(
+        tags=['Archivos'],
+        request=FileSerializer,
+        responses=FileSerializer,
+        operation_id='Muestra el listado de archivos',
+        description='Usado para mostrar los archivos de la tarea especificada como parámetro en la URL.',
+    )
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+    
+    @extend_schema(
+        tags=['Archivos'],
+        request=FileSerializer,
+        responses=FileSerializer,
+        operation_id='Guarda un Archivo cargado',
+        description='Usado para guardar el archivo de la ruta especificada como parámetro en la URL.',
+    )
+    def post(self, request, *args, **kwargs):
+        file = request.files['file']
+        file_uploaded = uploader.upload(
+            file,
+            folder='PML',
+            resource_type = 'image',
+            use_filemname = True
+            )
+        
+        file_url = file_uploaded['secure_url']
+        return self.create(request, *args, **kwargs)
 
 class FileDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (permissions.AllowAny, )
@@ -22,28 +55,8 @@ class FileDetail(generics.RetrieveUpdateDestroyAPIView):
         tags=['Archivos'],
         request=FileSerializer,
         responses=FileSerializer,
-        operation_id='Modificación del Archivo especificado',
-        description='Usado para modificar un equipo especificado como parámetro en la URL, debe proporcionar todos los valores del equipo.',
-    )
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    @extend_schema(
-        tags=['Archivos'],
-        request=FileSerializer,
-        responses=FileSerializer,
-        operation_id='Modificación parcial del Archivo especificado',
-        description='Modificación parcial del equipo especificado como parámetro en la URL. solo indique los valores que quiere modificar.',
-    )
-    def patch(self, request, *args, **kwargs):
-        return self.partial_update(request, *args, **kwargs)
-
-    @extend_schema(
-        tags=['Archivos'],
-        request=FileSerializer,
-        responses=FileSerializer,
         operation_id='Eliminación del Archivo especificado',
-        description='Elimina el equipo especificado como parámetro en la URL.',
+        description='Elimina el archivo especificado como parámetro en la URL.',
     )
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
