415e4f13835b38bf347e5cf08945febd81445c56
Isaias RoblesVega <isak4567@gmail.com>
2024-05-31 23:38:42 -0300

    dev-isaias: Rempalzo el contenido de la landing por uno mas real

--- Frontend/src/pages/landing.jsx
+++ Frontend/src/pages/landing.jsx
@@ -1,6 +1,17 @@
 import { Link } from "react-router-dom"
-import img1 from "../assets/img/template-img1.webp"
-import img2 from "../assets/img/template-img2.webp"
+import img1 from "../assets/img/trabajoEquipo.webp"
+import img2 from "../assets/img/todolist.webp"
+import logo from "../assets/img/logo.png"
+import cal from "../assets/img/calendario.png"
+import trabajo from "../assets/img/trabajo.png"
+import user from "../assets/img/usuario.png"
+import mundo from "../assets/img/mundo.png"
+import instagram from "../assets/svg/instagram.svg"
+import twitter from "../assets/svg/twitter.svg"
+import youtube from "../assets/svg/youtube.svg"
+import tiktok from "../assets/svg/tiktok.svg"
+
+
 
 
 const Landing = () => {
@@ -10,27 +21,40 @@ const Landing = () => {
         <header className="lHeader">
 
           <div className="lHeader__Logo">
-            <p>Site name</p>
+            <img src={logo} alt="" />
+            <div>
+              <p>
+                <span className="pLogo">P</span>roject
+              </p>
+              <p>
+                <span className="mLogo">M</span>anagment
+              </p>
+              <p>
+                <span className="lLogo">L</span>atam
+              </p>
+            </div>
           </div>
 
           <div className="lHeader__Nav">
-            <Link to={"/auth/login"}>Log In</Link>
-            <Link className="lHeader__Nav--SignUp" to={"/auth/register"}>Sign Up</Link>
+            <Link to={"/auth/login"}>Inicia sesión</Link>
+            <Link className="lHeader__Nav--SignUp" to={"/auth/register"}>Regístrate</Link>
           </div>
 
         </header>
 
         <div className="lSection1">
-          <h1>Landing page title</h1>
-          <p>And a subheading describing your site, too</p>
-          <button>button</button>
+          <h1>Gestión de Proyectos para Latinoamérica</h1>
+          <p>Simplifica la gestión de tus proyectos con nuestra herramienta innovadora.</p>
+          <button>
+            <Link to={"/auth/register"}>Empezar ahora</Link>
+          </button>
         </div>
 
         <div className="lSection2">
 
           <div>
-            <h2>Heading</h2>
-            <p>A subheading for this section, as long or as short as you like</p>
+            <h2>Colaboración Eficiente</h2>
+            <p>Mejora la comunicación y la colaboración en tu equipo con nuestras herramientas integradas.</p>
           </div>
 
           <img src={img1} alt="" />
@@ -40,8 +64,8 @@ const Landing = () => {
         <div className="lSection2 flex-reverse">
 
           <div>
-            <h2>Heading</h2>
-            <p>Another subheading—maybe it’s related to the image on the left, or the button below </p>
+            <h2>Gestión Visual</h2>
+            <p>Visualiza el progreso de tus proyectos con tableros intuitivos y personalizables.</p>
           </div>
 
           <img src={img2} alt="" />
@@ -50,28 +74,32 @@ const Landing = () => {
 
         <div className="lSection3">
 
-          <h2>Heading</h2>
+          <h2>Cualidades del proyecto</h2>
 
           <div className="lSection3__list">
 
             <div>
-              <h3>Subheading</h3>
-              <p>Body text for whatever you’d like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story. </p>
+              <img src={user} alt="" />
+              <h3>Tareas y Subtareas</h3>
+              <p>Organiza tus proyectos dividiéndolos en tareas y subtareas fácilmente manejables.</p>
             </div>
 
             <div>
-              <h3>Subheading</h3>
-              <p>Body text for whatever you’d like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story. </p>
+              <img src={cal} alt="" />
+              <h3>Calendarios y Plazos</h3>
+              <p>Mantén tus proyectos a tiempo con calendarios integrados y recordatorios de plazos.</p>
             </div>
 
             <div>
-              <h3>Subheading</h3>
-              <p>Body text for whatever you’d like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story. </p>
+              <img src={mundo} alt="" />
+              <h3>Integraciones</h3>
+              <p>Conecta con tus herramientas favoritas como Slack, Google Drive, y más.</p>
             </div>
 
             <div>
-              <h3>Subheading</h3>
-              <p>Body text for whatever you’d like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story. </p>
+              <img src={trabajo} alt="" />
+              <h3>Informes y Análisis</h3>
+              <p>Genera informes detallados y analiza el rendimiento de tu equipo en tiempo real.</p>
             </div>
 
           </div>
@@ -79,23 +107,78 @@ const Landing = () => {
         </div>
 
         <div className="lSection4">
-          <h2>Heading</h2>
-          <p>Plus a subheading for your site’s footer</p>
-          <button>button</button>
+          <h2>Comienza a mejorar la gestión de tus proyectos hoy</h2>
+          <p>Experimenta con unas de las mejores plataformas de gestion totalmente gratis</p>
+          <button>
+            <Link to={"/auth/register"}>Regístrate Gratis</Link>
+          </button>
+        </div>
+
+        <div className="lSection5">
+          <form className="lSection5__Contacto" method="post">
+            <p className="lSection5__Contacto--Titulo">Contáctanos</p>
+            <p className="lSection5__Contacto--Subtitulo">¿Tienes alguna pregunta? <br /> ¡Nos encantaría saber de ti!</p>
+
+            <div className="lSection5__Contacto__Group1">
+
+              <div className="form--group">
+                <label htmlFor="first-name">Tu nombre</label>
+                <input type="text" name="first-name" />
+              </div>
+
+              <div className="form--group">
+                <label htmlFor="last-name">Tu apellido</label>
+                <input type="text" name="last-name" />
+              </div>
+
+            </div>
+
+            <div className="form--group lSection5__Contacto__Group2 ">
+              <label htmlFor="email">Tu correo electronico</label>
+              <input type="email" name="email" />
+            </div>
+
+            <div className="lSection5__Contacto__Group3">
+              <label htmlFor="email">Tu Mensaje</label>
+              <textarea name="" id=""></textarea>
+            </div>
+
+            <button className="lSection5__Contacto--button" type="submit">Enviar</button>
+
+          </form>
+
+          <div className="lSection5--Imagen"></div>
         </div>
 
         <footer className="lFooter">
 
           <div className="lFooter__cont1">
 
-            <p>Site Name</p>
+            <div className="lHeader__Logo">
+              <img src={logo} alt="" />
+              <div>
+                <p>
+                  <span className="pLogo">P</span>roject
+                </p>
+                <p>
+                  <span className="mLogo">M</span>anagment
+                </p>
+                <p>
+                  <span className="lLogo">L</span>atam
+                </p>
+              </div>
+            </div>
 
-            <ul>
-              <li><a href="http://"><img src="" /></a></li>
-              <li><a href="http://"><img src="" /></a></li>
-              <li><a href="http://"><img src="" /></a></li>
-              <li><a href="http://"><img src="" /></a></li>
-            </ul>
+            <div className="lFooter__redes">
+              <p>Siguenos en nuestras redes:</p>
+              <ul>
+                <li><a href="http://"><img src={instagram} /></a></li>
+                <li><a href="http://"><img src={twitter} /></a></li>
+                <li><a href="http://"><img src={youtube} /></a></li>
+                <li><a href="http://"><img src={tiktok} /></a></li>
+              </ul>
+            </div>
+
 
           </div>
 
@@ -103,22 +186,22 @@ const Landing = () => {
 
             <div className="lFooter__cont2__items">
 
-              <p>Topic</p>
+              <p>Emails</p>
               <ul>
-                <li><a href="http://">Page</a></li>
-                <li><a href="http://">Page</a></li>
-                <li><a href="http://">Page</a></li>
+                <li><a href="http://">Pmlatam@gmail.com</a></li>
+                <li><a href="http://">Pmlatam@hotmail.com.ar</a></li>
               </ul>
 
             </div>
 
             <div className="lFooter__cont2__items">
 
-              <p>Topic</p>
+              <p>Sobre Nosotros</p>
               <ul>
-                <li><a href="http://">Page</a></li>
-                <li><a href="http://">Page</a></li>
-                <li><a href="http://">Page</a></li>
+                <li><a href="http://">Blog</a></li>
+                <li><a href="http://">Soporte</a></li>
+                <li><a href="http://">Política de Privacidad</a></li>
+                <li><a href="http://">Términos de Servicio</a></li>
               </ul>
               
             </div>
