6847cd04206b502206b2dc1339b0b3ca0ff1b1c9
cometas321 <renerolando321@gmail.com>
2024-06-01 20:42:30 -0500

    rutas task avanzado

--- Backend/tasks/views.py
+++ Backend/tasks/views.py
@@ -12,19 +12,18 @@ class TaskList(generics.ListCreateAPIView):
     @extend_schema(
         tags=['Tasks'],
         operation_id='creacion de tareas',
-        description='se crea una nueva tarea',
+        description='Se crea una nueva tarea',
     )
     def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+        return self.create(request, *args, **kwargs)    
     
     @extend_schema(
         tags=['Tasks'],
         operation_id='listado de tareas',
-        description='se listan todas las tareas',
+        description='Se listan todas las tareas',
     )
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
-
 
 class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [permissions.AllowAny]
@@ -33,19 +32,24 @@ class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
 
     @extend_schema(
         tags=['Tasks'],
-        operation_id='actualizar tareas',
-        description='se actualiza una tarea',
+        operation_id='actualizar tarea',
+        description='Se actualiza una tarea',
     )
     def put(self, request, *args, **kwargs):
         return self.update(request, *args, **kwargs)
 
     @extend_schema(
         tags=['Tasks'],
-        operation_id='borrar tareas',
-        description='se elimina una tarea',
+        operation_id='actualizar parcialmente tarea',
+        description='Se actualiza parcialmente una tarea',
+    )
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Tasks'],
+        operation_id='borrar tarea',
+        description='Se elimina una tarea',
     )
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
-
-
-
