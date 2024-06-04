6d63d892bb02d1fa096845f327f76acd862ccdd8
Fernando <fer.gab.sua@gmail.com>
2024-06-04 19:20:08 -0300

    correcion url

--- Backend/projects/api/urls.py
+++ Backend/projects/api/urls.py
@@ -6,17 +6,3 @@ urlpatterns = [
     path('list/',ProjectListCreateApiView.as_view(), name = 'project_list' ),
     path('detail/<int:pk>/',ProjectDetailCreateApiView.as_view(), name = 'project_detail' ),
 ]
-
-
-"""
-urlpatterns = [
-    path('list/', ProjectListView.as_view(), name='project-list'), #ok
-    path('list/', ProjectCreateView.as_view(), name='project-create'), #ok
-    path('detail/<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),# ok
-    path('detail/<int:pk>/', ProjectUpdateView.as_view(), name='project-update'), #ok
-    path('detail/<int:pk>/', ProjectDeleteView.as_view(), name='project-delete'), #ok
-]
-
-"""
-
-
